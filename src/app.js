2f332c19b9247c6beaaa73857ece2698dcdc505d
gusleak <kwnstantinos.pz@gmail.com>
2021-04-03 12:02:55 +0200

    Export forecast from separate file

--- src/app.js
+++ src/app.js
@@ -3,26 +3,8 @@ class App extends React.Component {
         super(props);
         this.state = { 
             theme: 'light-theme',
-            themeChecked: false,
-            weatherData: '',
-            dates: '',
-            tempToday: '',
-            tempTomorrow: '',
-            tempDayAfter: ''
+            themeChecked: false
         };
-    }
-
-    componentDidMount() {
-        fetch('https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.3913&lon=5.3221')
-            .then(res => res.json())
-            .then(json => 
-                this.setState({
-                    dates: getDates(json.properties.timeseries[0].time),
-                    tempToday: [getTemp('min', 0, json.properties.timeseries), getTemp('max', 0, json.properties.timeseries)],
-                    tempTomorrow: [getTemp('min', 1, json.properties.timeseries), getTemp('max', 1, json.properties.timeseries)],
-                    tempDayAfter: [getTemp('min', 2, json.properties.timeseries), getTemp('max', 2, json.properties.timeseries)],
-                    weatherData: json.properties.timeseries
-            }))
     }
 
     render() {
@@ -37,36 +19,7 @@ class App extends React.Component {
                     <span className='slider round'></span>
                 </div>
                 <NavBar />
-                <table id='weather-table' className='table table-borderless'>
-                    <thead>
-                        <tr>
-                            <th><i className="fas fa-calendar-day"></i></th>
-                            <th><i className="fas fa-thermometer-half"></i></th>
-                            <th><i className="fas fa-wind"></i></th>
-                            <th><i className="fas fa-umbrella"></i></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{this.state.dates[0]}</td>
-                            <td>{this.state.tempToday[0]} &#8451; til {this.state.tempToday[1]} &#8451;</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>{this.state.dates[1]}</td>
-                            <td>{this.state.tempTomorrow[0]} &#8451; til {this.state.tempTomorrow[1]} &#8451;</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>{this.state.dates[2]}</td>
-                            <td>{this.state.tempDayAfter[0]} &#8451; til {this.state.tempDayAfter[1]} &#8451;</td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Weather />
             </div>
         );
     }
@@ -84,33 +37,6 @@ function NavBar() {
         </ul>
     );
 }
-
-const getDates = (isoDate) => {
-    const today = new Date(isoDate);
-    let tomorrow = new Date();
-    tomorrow.setDate(today.getDate() + 1);
-    let dayAfter = new Date();
-    dayAfter.setDate(today.getDate() + 2);
-    const settings = { weekday:'long', day: 'numeric', month: 'long' }
-    return [today.toLocaleDateString('no', settings), tomorrow.toLocaleDateString('no', settings), dayAfter.toLocaleDateString('no', settings)];
-}
-
-const getTemp = (val, dayIdx, dataArr) => {
-    return val === 'min' 
-    ? Math.min.apply(Math, dataArr.map(data => {
-        const currentDate = new Date(data.time).getDate();
-        const queriedDate = new Date(dataArr[0].time);
-        if (new Date(queriedDate.setDate(queriedDate.getDate() + dayIdx)).getDate() === currentDate) {
-            return data.data.instant.details.air_temperature
-        }}).filter(Number))
-    : Math.max.apply(Math, dataArr.map(data => {
-        const currentDate = new Date(data.time).getDate();
-        const queriedDate = new Date(dataArr[0].time);
-        if (new Date(queriedDate.setDate(queriedDate.getDate() + dayIdx)).getDate() === currentDate) {
-            return data.data.instant.details.air_temperature
-        }}).filter(Number))
-}
-
 
 const element = <App city="Bergen" />;
 
